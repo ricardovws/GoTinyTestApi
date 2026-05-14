385dbbd160f3666707f7d0674f8e289a5ae5f9d7
ricardovws <ricardovws@gmail.com>
2026-05-14 19:33:52 -0300

    update code

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Heeey there, welcome to my simple test api :)~")
+	fmt.Fprintf(w, "Heeey there, welcome to my simple test api written in Go!")
 }
 
 func main() {
